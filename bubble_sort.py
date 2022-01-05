63165c452209a7628bf8923e15fa18a6ffdca2ae
Sreejith Vidyadharan <sreejithvs1167@gmail.com>
2022-01-05 06:57:37 +0000

    Added bubble sort method with a new range func parameters

--- bubble_sort.py
+++ bubble_sort.py
@@ -10,3 +10,13 @@ def bubble(sequence):
     return sequence
 
 print(bubble(test_sequence))
+
+
+def bubble_sort(sequence):
+    for i in range(len(sequence)-1, 0, -1):
+        for j in range(i):
+            if sequence[j] > sequence[j+1]:
+                sequence[j], sequence[j+1] = sequence[j+1], sequence[j]
+    return sequence
+
+print(bubble_sort(test_sequence))
